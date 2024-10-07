8fe896e7746ad67f423a980a30de258d609f4cf6
WellZaia <bassosprey2809@gmail.com>
2024-10-06 23:50:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Arte',
+    'O que é Mangá?',
+    'Mangá é o nome dado a histórias em quadrinhos japonesas. Os mangás possuem características marcantes nos seus desenhos, como olhos acentuados e expressivos. Existem vários tipos e gêneros de mangás.'
+)
+
+criaCartao(
+    'Calvície',
+    'Por que o Carlos é calvo?',
+    'No caso da alopecia androgenética, esse hormônio provoca mudanças nos folículos capilares, deixando-os mais finos, curtos e sem pigmentação. Daí a impressão de que o cabelo está caindo, ficando mais “rarefeito”. Esse processo começa logo após a puberdade, quando se inicia o estímulo hormonal.'
+)
+
+criaCartao(
+    'Goku',
+    'Como o Goku chega na forma ultra instinto?',
+    'Como funciona o Ultra Instinct? O Ultra Instinct funciona através da eliminação de qualquer pensamento consciente durante a batalha. Isso permite que Goku acesse todo o seu potencial de luta sem ser limitado por suas próprias dúvidas ou medos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa falar peixe bola gato?',
+    '"fish", "ball" e "cat"'
+)
